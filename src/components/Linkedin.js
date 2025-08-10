f3a8417722e92c970d83ec3342bed9066c7156a3
Akkash Dharsaun <akkashdharsaun02@gmail.com>
2025-08-10 22:27:57 +0530

    Initial commit

---
+++ src/components/Linkedin.js
@@ -0,0 +1,11 @@
+export const Linkedin = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.width}
+    height={props.height}
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path d="M4.98 3.5C4.98 4.88 3.88 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM.5 8h4V24h-4V8zm7.5 0h3.8v2.2h.05c.53-1 1.84-2.05 3.8-2.05 4.06 0 4.8 2.67 4.8 6.13V24h-4v-7.9c0-1.88-.03-4.3-2.62-4.3-2.62 0-3.02 2.05-3.02 4.15V24h-4V8z" />
+  </svg>
+);
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,84 @@
+import { motion } from "framer-motion";
+import React from "react";
+// import { NavLink } from 'react-router-dom'
+import styled from "styled-components";
+import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+import { Linkedin } from "../components/Linkedin";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;  
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled(motion.span)`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
+
+const socialLinks = [
+  {
+    icon: <Github width={25} height={25} />,
+    link: "https://github.com/AkkashDharsaun"
+  },
+  {
+    icon: <Twitter width={25} height={25} />,
+    link: "https://twitter.com/code_bucks"
+  },
+  {
+    icon: <Linkedin width={25} height={25} />,
+    link: "https://www.linkedin.com/in/akash-dharsaun-34a17429a"
+  }
+]
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      {socialLinks.map((social, index) => (
+        <motion.div
+          key={index}
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", duration: 1, delay: index * 0.2 }}
+        >
+          <a
+            style={{ color: "inherit" }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={social.link}
+          >
+            {social.icon}
+          </a>
+        </motion.div>
+      ))}
+      <Line
+        color={props.theme}
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1,
+          delay: 0.8,
+        }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
